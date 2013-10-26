6d61fa15f5dc7b3c82080d05b311d189abed9454
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-10-26 14:55:58 +0200

    simple json database

---
+++ db.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+
+	"github.com/knakk/intset"
+)
+
+// DB is a simple database backed by a map and a mutex.
+type DB struct {
+	docs map[int][]byte
+	sync.RWMutex
+	idMax int           // autoincremented ID
+	all   intset.IntSet // keep an index of all doc IDs
+}
+
+type doc struct {
+	ID   int
+	Data json.RawMessage
+}
+
+// New returns a new database.
+func New(size int) *DB {
+	return &DB{
+		docs: make(map[int][]byte, size),
+		all:  intset.New(),
+	}
+}
+
+// NewFromFile loads db from file into memory and return it as a new database.
+func NewFromFile(fname string) (*DB, error) {
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		docs  []doc
+		db    = New(len(docs))
+		bcopy []byte
+	)
+	err = json.Unmarshal(b, &docs)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, d := range docs {
+		err := d.Data.UnmarshalJSON(bcopy)
+		if err != nil {
+			return nil, err
+		}
+		db.Set(d.ID, &bcopy)
+	}
+	db.Lock()
+	defer db.Unlock()
+	return db, nil
+}
+
+// Size returns the size of the database
+func (db *DB) Size() int {
+	db.RLock()
+	defer db.RUnlock()
+	return db.all.Size()
+}
+
+// Create inserts a new document into the database. It returns the id of the
+// created document.
+func (db *DB) Create(data *[]byte) int {
+	db.Lock()
+	defer db.Unlock()
+	db.idMax++
+	db.docs[db.idMax] = *data
+	db.all.Add(db.idMax)
+	return db.idMax
+}
+
+// Get returns a document by a given id.
+func (db *DB) Get(id int) (*[]byte, error) {
+	db.RLock()
+	defer db.RUnlock()
+	if b, ok := db.docs[id]; ok {
+		return &b, nil
+	}
+	return nil, errors.New("document not found")
+}
+
+// Set updates a document at agive id.
+func (db *DB) Set(id int, data *[]byte) {
+	db.Lock()
+	defer db.Unlock()
+	db.docs[id] = *data
+	// Make sure ID is in the set. Needed when a DB is loaded from file.
+	db.all.Add(id)
+	if id > db.idMax {
+		db.idMax = id
+	}
+}
+
+// All retuns all the docs in the database as a JSON array, in the form:
+// [{"ID": 1, "Data": {jsonData}},{..},{..}]
+func (db *DB) All() []byte {
+	var allDocs bytes.Buffer
+	allDocs.Write([]byte("["))
+	db.RLock()
+	defer db.RUnlock()
+	size := db.Size()
+	i := 0
+	for k := range db.all {
+		allDocs.Write([]byte(fmt.Sprintf("{\"ID\":%v,\"Data\":", k)))
+		allDocs.Write(db.docs[k])
+		allDocs.Write([]byte("}"))
+		i++
+		if i != size { // to avoid a trailing comma after last doc
+			allDocs.Write([]byte(","))
+		}
+	}
+	allDocs.Write([]byte("]"))
+	return allDocs.Bytes()
+}
+
+// Dump dumps the DB into a file.
+func (db *DB) Dump(fname string) error {
+	f, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(db.All())
+	return err
+}
+
+// func (db *DB) GetSeveral(docs Intset) []byte
---
+++ db_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/knakk/specs"
+)
+
+type Book struct {
+	Author, Title string
+	Issued        int
+}
+
+func TestCRUD(t *testing.T) {
+	s := specs.New(t)
+
+	// new DB
+	db := New(32)
+	// DB size
+	s.Expect(db.Size(), 0)
+	// doc not found
+	data, err := db.Get(99)
+	s.Expect(err.Error(), "document not found")
+
+	// create doc
+	book, err := json.Marshal(Book{"Knut Hamsun", "Sult", 1890})
+	s.ExpectNilFatal(err)
+	id := db.Create(&book)
+	s.Expect(db.Size(), 1)
+	s.ExpectNot(id, 0)
+
+	// fetch doc
+	data, err = db.Get(id)
+	s.ExpectNilFatal(err)
+	var b Book
+	err = json.Unmarshal(*data, &b)
+	s.ExpectNilFatal(err)
+	s.Expect(b.Author, "Knut Hamsun")
+	s.Expect(b.Title, "Sult")
+	s.Expect(b.Issued, 1890)
+
+	// update (set) doc
+	book2, err := json.Marshal(Book{"Knut Hamsun", "Pan", 1994})
+	s.ExpectNilFatal(err)
+	id2 := db.Create(&book2)
+	book3, err := json.Marshal(Book{"Knut Hamsun", "Pan", 1894})
+	s.ExpectNilFatal(err)
+	db.Set(id2, &book3)
+	data, err = db.Get(id2)
+	s.ExpectNilFatal(err)
+	err = json.Unmarshal(*data, &b)
+	s.Expect(b.Issued, 1894)
+
+	// get all docs
+	type all []struct {
+		ID   int
+		Data Book
+	}
+	allb := db.All()
+	var allj all
+	err = json.Unmarshal(allb, &allj)
+	s.ExpectNilFatal(err)
+	s.Expect(db.Size(), len(allj))
+	s.Expect(allj[0].Data.Author, "Knut Hamsun")
+
+	// save db
+	err = db.Dump("all.json")
+	s.ExpectNilFatal(err)
+
+	// load db
+	db2, err := NewFromFile("all.json")
+	s.ExpectNilFatal(err)
+	s.Expect(db2.Size(), 2)
+
+	err = db.Dump("cpy.json")
+	s.ExpectNilFatal(err)
+
+	// compare the db files to make sure they are equal
+	f1, err := os.Open("all.json")
+	s.ExpectNilFatal(err)
+	defer f1.Close()
+	f2, err := os.Open("cpy.json")
+	s.ExpectNilFatal(err)
+	defer f2.Close()
+	h1 := sha256.New()
+	h2 := sha256.New()
+	_, err = io.Copy(h1, f1)
+	s.ExpectNilFatal(err)
+	_, err = io.Copy(h2, f2)
+	s.ExpectNilFatal(err)
+	s.Expect(fmt.Sprintf("% x", h1.Sum(nil)), fmt.Sprintf("% x", h2.Sum(nil)))
+
+	// delete dbs
+	err = os.Remove("all.json")
+	s.ExpectNilFatal(err)
+	err = os.Remove("cpy.json")
+	s.ExpectNilFatal(err)
+
+}
